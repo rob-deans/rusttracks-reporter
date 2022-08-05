5d947a6f74c6bd8274cfb1c9d9bc6e38d90c93c5
Rob Deans <17593196+rob-deans@users.noreply.github.com>
2022-08-05 20:01:46 +0100

    js updates and changing sql type

--- rusttracks-contrib/src/utils.rs
+++ rusttracks-contrib/src/utils.rs
@@ -2,6 +2,6 @@ use chrono::{NaiveDateTime};
 use chrono::format::ParseError;
 
 pub fn date_to_unixts(date: String) -> Result<i64, ParseError> {
-    let datetime = NaiveDateTime::parse_from_str(&date, "%Y-%m-%d %H:%M:%S")?.timestamp();
+    let datetime = NaiveDateTime::parse_from_str(&date, "%Y-%m-%dT%H:%M")?.timestamp();
     Ok(datetime)
 }
