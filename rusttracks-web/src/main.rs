8b28b75c27952158e26efd4e3fc6c29c81f333b1
Rob Deans <17593196+rob-deans@users.noreply.github.com>
2022-08-08 22:50:43 +0100

    Tidy up main.rs

--- rusttracks-web/src/main.rs
+++ rusttracks-web/src/main.rs
@@ -1,21 +1,14 @@
 extern crate openssl;
-#[macro_use] extern crate diesel;
+extern crate diesel;
 extern crate diesel_migrations;
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate rocket_sync_db_pools;
 
 pub mod routes;
-// pub mod utils;
-// pub mod schema;
-// pub mod models;
 
-use std::{env, process, format, time::Duration};
+use std::env;
 use rocket::figment::{value::{Map, Value}, util::map};
 use rocket::fs::{FileServer, relative};
-use self::diesel::prelude::*;
-use rocket::serde::{json::Json};
-use rusttracks_contrib::models::LocationPayload;
-use rusttracks_contrib::schema;
 
 #[database("owntracks")]
 pub struct OwntracksDB(diesel::SqliteConnection);
@@ -35,10 +28,9 @@ async fn main() -> Result<(), rocket::Error> {
         .merge(("databases", map!["owntracks" => db]));
 
 
-    let rocket = rocket::custom(figment)
+    let _ = rocket::custom(figment)
         .attach(OwntracksDB::fairing())
-        // .mount("/", routes![routes::hello])
-        .mount("/", FileServer::from(relative!("static")))
+        .mount("/", FileServer::from(relative!("dist")))
         .mount("/api", routes![routes::get_locations])
         .launch()
         .await?;
